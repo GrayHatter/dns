b4ea5ac13929047aa9df2e895fc6bbc4de256e9a
Gregory Mullen <_@gr.ht>
2025-03-07 09:09:34 -0800

    clean build.zig

--- build.zig
+++ build.zig
@@ -10,38 +10,29 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const dnsc_mod = b.createModule(.{
+    const client_mod = b.createModule(.{
         .root_source_file = b.path("src/client.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const dnsd_mod = b.createModule(.{
+    const daemon_mod = b.createModule(.{
         .root_source_file = b.path("src/server.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const exe_c = b.addExecutable(.{
-        .name = "dnsc",
-        .root_module = dnsc_mod,
-    });
+    const client = b.addExecutable(.{ .name = "dnsc", .root_module = client_mod });
+    b.installArtifact(client);
 
-    b.installArtifact(exe_c);
-
-    const run_cmd = b.addRunArtifact(exe_c);
-
+    const run_cmd = b.addRunArtifact(client);
     run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
+    if (b.args) |args| run_cmd.addArgs(args);
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
     const test_step = b.step("test", "Run unit tests");
-    inline for (.{ dns_mod, dnsc_mod, dnsd_mod }) |mod| {
+    inline for (.{ dns_mod, client_mod, daemon_mod }) |mod| {
         const tests = b.addTest(.{ .root_module = mod });
         const test_run = b.addRunArtifact(tests);
         test_step.dependOn(&test_run.step);
