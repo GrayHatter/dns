4f5d71d88403937a5648a8561078d8a414a5417d
Gregory Mullen <_@gr.ht>
2025-11-20 20:52:11 -0800

    move upstream into Upstream.zig

--- src/server.zig
+++ src/server.zig
@@ -1,47 +1,3 @@
-const Upstream = struct {
-    conns: [4]net.Stream,
-    next: u8 = 0,
-
-    const Peer = struct {
-        addr: Io.net.IpAddress,
-
-        pub const cloudflare_0: Peer = .{ .addr = .{ .ip4 = .{ .bytes = .{ 1, 1, 1, 1 }, .port = 53 } } };
-        pub const cloudflare_1: Peer = .{ .addr = .{ .ip4 = .{ .bytes = .{ 1, 0, 0, 1 }, .port = 53 } } };
-        pub const google_0: Peer = .{ .addr = .{ .ip4 = .{ .bytes = .{ 8, 8, 8, 8 }, .port = 53 } } };
-        pub const google_1: Peer = .{ .addr = .{ .ip4 = .{ .bytes = .{ 8, 8, 4, 4 }, .port = 53 } } };
-    };
-    const peers: [4]Peer = .{
-        .cloudflare_0,
-        .cloudflare_1,
-        .google_0,
-        .google_1,
-    };
-
-    pub fn init(up: *Upstream, io: Io) !void {
-        for (&up.conns, peers) |*stream, peer| {
-            stream.* = try peer.addr.connect(io, .{ .mode = .dgram, .protocol = .udp });
-        }
-    }
-
-    pub fn get(up: *Upstream) struct { Peer, net.Stream } {
-        var next = @atomicLoad(u8, &up.next, .unordered);
-        while (true) {
-            if (@cmpxchgWeak(
-                u8,
-                &up.next,
-                next,
-                @intCast((next + 1) % up.conns.len),
-                .monotonic,
-                .monotonic,
-            )) |new| {
-                next = new;
-            } else {
-                return .{ peers[next], up.conns[next] };
-            }
-        }
-    }
-};
-
 var upstreams: Upstream = .{ .conns = undefined };
 
 pub const std_options: std.Options = .{
@@ -725,6 +681,8 @@ test parseHostFile {
 test main {
     _ = &main;
 }
+
+const Upstream = @import("Upstream.zig");
 
 const DNS = @import("dns.zig");
 const RData = DNS.Message.Resource.RData;
